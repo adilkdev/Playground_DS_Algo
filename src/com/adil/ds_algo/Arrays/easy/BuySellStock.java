9b580c92e57143581e7ba1b79a52ea435e6b06c9
adilkdev <adilkdev@gmail.com>
2022-03-02 09:05:25 +0530

    Buy sell stock question description

--- src/com/adil/ds_algo/Arrays/easy/BuySellStock.java
+++ src/com/adil/ds_algo/Arrays/easy/BuySellStock.java
@@ -1,5 +1,12 @@
 package com.adil.ds_algo.Arrays.easy;
 
+/**
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ * Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+ */
+
 public class BuySellStock {
 
     public int maxProfit(int[] prices) {
