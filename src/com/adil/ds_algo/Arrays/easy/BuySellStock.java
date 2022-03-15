8f0412b1f5018f9d4beafd2d2977d1c102def7c5
Adil <adilkdev@gmail.com>
2022-03-15 07:23:43 +0530

    added comment for buy sell stock

--- src/com/adil/ds_algo/Arrays/easy/BuySellStock.java
+++ src/com/adil/ds_algo/Arrays/easy/BuySellStock.java
@@ -10,7 +10,10 @@ package com.adil.ds_algo.Arrays.easy;
 public class BuySellStock {
 
     public int maxProfit(int[] prices) {
-        int buy=Integer.MAX_VALUE,maxProfit=0;
+        /**
+         * we need to buy the stock when the price is minimum.
+         */
+        int buy=Integer.MAX_VALUE, maxProfit=0;
         for(int i=0;i<prices.length;i++){
             buy=Math.min(buy,prices[i]);
             maxProfit=Math.max(maxProfit,prices[i]-buy);
