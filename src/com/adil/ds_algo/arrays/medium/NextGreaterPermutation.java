626a8d8fa8eda867a70af6b3180cafde7b76cc0c
adilkdev <adilkdev@gmail.com>
2022-04-03 09:21:47 +0530

    next greater permutation

--- src/com/adil/ds_algo/arrays/medium/NextGreaterPermutation.java
+++ src/com/adil/ds_algo/arrays/medium/NextGreaterPermutation.java
@@ -5,10 +5,13 @@ public class NextGreaterPermutation {
     public void nextPermutation(int[] nums) {
         if (nums.length == 1)
             return;
-        if (nums.length == 2)
+        if (nums.length == 2) {
             swap(nums, 0, 1);
-        int dec = nums.length -2;
-        while (dec >=0 && nums[dec] > nums[dec + 1])
+            return;
+        }
+
+        int dec = nums.length - 2;
+        while (dec >=0 && nums[dec] >= nums[dec + 1])
             dec--;
         reverse(nums, dec + 1, nums.length - 1);
 
@@ -36,4 +39,5 @@ public class NextGreaterPermutation {
             end--;
         }
     }
+
 }
