3582578f71eed4814db591bd7a64098d853f3a80
adilkdev <adilkdev@gmail.com>
2022-04-03 09:25:52 +0530

    added comments for the problem

--- src/com/adil/ds_algo/arrays/medium/NextGreaterPermutation.java
+++ src/com/adil/ds_algo/arrays/medium/NextGreaterPermutation.java
@@ -1,5 +1,19 @@
 package com.adil.ds_algo.arrays.medium;
 
+/**
+ * A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+ *
+ *     For example, for arr = [1,2,3], the following are considered permutations of arr:
+ *     [1,2,3], [1,3,2], [2,1,3], [2,3,1]
+ *     The next permutation of an array of integers is the next lexicographically greater permutation
+ *     of its integer.
+ *
+ *    For example, the next permutation of arr = [1,2,3] is [1,3,2].
+ *    Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+ *    While the next permutation of arr = [3,2,1] is [1,2,3]
+ *    because [3,2,1] does not have a lexicographical larger rearrangement.
+ */
+
 public class NextGreaterPermutation {
 
     public void nextPermutation(int[] nums) {
