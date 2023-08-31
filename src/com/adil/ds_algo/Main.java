c8c72e2db67c67b681363d9b8cb1ca975cec1b6b
adilkdev <adilkdev@gmail.com>
2023-08-31 18:32:23 +0530

    More problems solved

--- src/com/adil/ds_algo/Main.java
+++ src/com/adil/ds_algo/Main.java
@@ -1,7 +1,7 @@
 package com.adil.ds_algo;
 
 import com.adil.ds_algo.arrays.easy.MoveAllZerosToEnd;
-import com.adil.ds_algo.arrays.medium.MaxSubArray;
+import com.adil.ds_algo.arrays.easy.Sort01;
 
 import java.util.Arrays;
 
